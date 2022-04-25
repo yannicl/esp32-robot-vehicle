5a4a8c68ec15051653c6a39f3ff0177e39513a54
Yannic Luyckx <yannic.luyckx@gmail.com>
2022-04-24 21:27:17 -0400

    app initial version with vl53l0x readings

---
+++ app/start.py
@@ -0,0 +1,18 @@
+from machine import Pin, SoftI2C
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=400000)
+i2c.scan()
+
+from third_party import VL53L0X
+tof = VL53L0X.VL53L0X(i2c)
+
+tof.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 18)
+
+tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 14)
+
+
+while True:
+# Start ranging
+    tof.start()
+    tof.read()
+    print(tof.read())
+    tof.stop()
