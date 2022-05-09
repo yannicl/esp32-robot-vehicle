eb84409ba63c1707bd5b39d5796c374c04e3b153
Yannic Luyckx <yannic.luyckx@gmail.com>
2022-05-08 22:19:32 -0400

    quick fix

--- app/radio.py
+++ app/radio.py
@@ -3,10 +3,11 @@ import socket
 class Radio:
 
     localIP     = "0.0.0.0"
+    broadcoastIP = "255.255.255.255"
     UDP_LOCAL_PORT   = 20935
 
     def __init__(self):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
     def send(self, message):
-        self.socket.sendto(message, (self.localIP, self.UDP_LOCAL_PORT))
+        self.socket.sendto(message, (self.broadcoastIP, self.UDP_LOCAL_PORT))
