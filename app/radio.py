32040fa82947e49dd3ad0ade53078ad7639803f6
Yannic Luyckx <yannic.luyckx@gmail.com>
2022-05-08 21:53:39 -0400

    add debugging through udp

---
+++ app/radio.py
@@ -0,0 +1,15 @@
+import socket
+
+class Radio:
+
+    localIP     = "0.0.0.0"
+    UDP_LOCAL_PORT   = 20935
+
+    def __init__(self):
+        self.socket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    def send(self, message):
+        if self.lastClientAddress:
+            self.socket.sendto(message, (self.localIP, self.UDP_LOCAL_PORT))
+
+
--- app/start.py
+++ app/start.py
@@ -1,33 +1,42 @@
 from machine import Pin, SoftI2C
 import time
-i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000)
-i2c.scan()
+import json
 
-from third_party import VL53L0X
-tof = VL53L0X.VL53L0X(i2c)
+try:
 
-tof.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 18)
+    i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000)
+    i2c.scan()
 
-tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 14)
+    from third_party import VL53L0X
+    tof = VL53L0X.VL53L0X(i2c)
+
+    tof.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 18)
+
+    tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 14)
 
 
-#while True:
-# Start ranging
-tof.start()
-tof.read()
-print(tof.read())
-tof.stop()
+    #while True:
+    # Start ranging
+    tof.start()
+    tof.read()
+    print(tof.read())
+    tof.stop()
 
-from app.goplus2 import GoPlus2
-motor = GoPlus2(i2c)
-motor.writeMotorASpeed(10000)
-motor.writeServo1Angle(50)
-time.sleep(4)
-motor.writeServo1Angle(0)
-motor.writeMotorASpeed(0)
-time.sleep(4)
-motor.writeServo1Angle(150)
-motor.writeMotorASpeed(-10000)
-time.sleep(4)
-motor.writeServo1Angle(0)
-motor.writeMotorASpeed(0)
+    from app.goplus2 import GoPlus2
+    motor = GoPlus2(i2c)
+    motor.writeMotorASpeed(10000)
+    motor.writeServo1Angle(50)
+    time.sleep(4)
+    motor.writeServo1Angle(0)
+    motor.writeMotorASpeed(0)
+    time.sleep(4)
+    motor.writeServo1Angle(150)
+    motor.writeMotorASpeed(-10000)
+    time.sleep(4)
+    motor.writeServo1Angle(0)
+    motor.writeMotorASpeed(0)
+
+except Exception as err:
+    from app.radio import Radio
+    radio = Radio()
+    radio.send(json.dump(err))
