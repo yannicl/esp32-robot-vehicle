452049cbd1fded1510081417a90551492d4eae1d
Yannic Luyckx <yannic.luyckx@gmail.com>
2022-05-15 23:03:48 -0400

    disable tof sensor and add color sensor

--- app/start.py
+++ app/start.py
@@ -14,21 +14,17 @@ while True:
         i2c = SoftI2C(scl=Pin(22), sda=Pin(21), freq=100000)
         i2c.scan()
 
-        from third_party import VL53L0X
-        tof = VL53L0X.VL53L0X(i2c)
-        tof.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 18)
-        tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 14)
-
-
-        #while True:
-        # Start ranging
-        #tof.start()
-        #tof.read()
-        #print(tof.read())
-        #tof.stop()
+        #from third_party import VL53L0X
+        #tof = VL53L0X.VL53L0X(i2c)
+        #tof.set_Vcsel_pulse_period(tof.vcsel_period_type[0], 18)
+        #tof.set_Vcsel_pulse_period(tof.vcsel_period_type[1], 14)
 
         from app.goplus2 import GoPlus2
         motor = GoPlus2(i2c)
+
+        from app.TCS34725 import TCS34725
+
+        colorSensor = TCS34725(i2c)
 
         while True:
             wdt.feed()
@@ -45,11 +41,16 @@ while True:
             else:
                 motor.writeMotorBSpeed(0)
             
-            tof.start()
-            dist = tof.read()
-            tof.stop()
+            #tof.start()
+            #dist = tof.read()
+            #tof.stop()
+            #r.send(json.dumps({
+            #'distance': dist
+            #}))
+
+            data = colorSensor.read(raw=True)
             r.send(json.dumps({
-            'distance': dist
+            'color': data
             }))
 
     except Exception as err:
